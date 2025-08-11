e09cec9f4f271b5e0090cc64f0dac2b812d492a5
rafay-narayana <narayana.reddy@rafay.co>
2025-08-11 16:56:19 +0530

    Your commit message

--- ns/main.tf
+++ ns/main.tf
@@ -1,11 +1,12 @@
-data "rafay_download_kubeconfig" "kubeconfig_cluster" {
-  cluster = var.cluster_name
-}
 
 resource "rafay_download_kubeconfig" "kubeconfig_cluster" {
   cluster            = var.cluster_name
   output_folder_path = "/tmp"
   filename           = "kubeconfig"
+}
+
+data "rafay_download_kubeconfig" "kubeconfig_cluster" {
+  cluster = var.cluster_name
 }
 
 resource "kubernetes_namespace" "ns" {
