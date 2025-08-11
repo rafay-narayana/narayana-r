939d6379549c7a66fae050c1230ddaabc8f89844
rafay-narayana <narayana.reddy@rafay.co>
2025-08-11 17:21:20 +0530

    Your commit message

--- ns/variables.tf
+++ ns/variables.tf
@@ -8,7 +8,7 @@ variable "host_cluster_name" {
   description = "The name of the cluster"
   type        = string
   validation {
-    condition     = length(var.cluster_name) > 0
+    condition     = length(var.host_cluster_name) > 0
     error_message = "The cluster name must not be empty."
   }
 }
