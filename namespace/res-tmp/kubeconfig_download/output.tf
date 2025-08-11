59f6cbac456f97f5f71b3e6e75ebe018b7e15653
rafay-narayana <narayana.reddy@rafay.co>
2025-08-11 11:35:49 +0530

    confing download Added

--- namespace/res-tmp/kubeconfig_download/output.tf
+++ namespace/res-tmp/kubeconfig_download/output.tf
@@ -1,3 +1,4 @@
-output "kubeconfig" {
-  value = rafay_download_kubeconfig.tfkubeconfig
+output "kubeconfig_cluster" {
+  description = "kubeconfig_cluster"
+  value       = data.rafay_download_kubeconfig.kubeconfig_cluster.kubeconfig
 }
